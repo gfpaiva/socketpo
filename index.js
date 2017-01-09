150f79bf9f93e9d1c9d778e6cf58fac4ff1d8fae
Guilherme Fernandes de Paiva <gf.paiva@yahoo.com.br>
2017-01-09 19:37:47 -0200

    Server start, first endpoint test, page errors

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+	consign = require('consign'),
+	bodyParser = require('body-parser'),
+	compression = require('compression'),
+	cors = require('cors'),
+	helmet = require('helmet'),
+	socket = require('socket.io'),
+	app = express(),
+	server = require('http').Server(app),
+	io = socket(server);
+
+app.set('port', 3000);
+app.set("json spaces", 4);
+app.use(helmet());
+app.use(cors({
+	origin: ["http://localhost:3000"],
+	methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+app.use(compression());
+app.use(bodyParser.json());
+
+consign({verbose: false})
+	.include('libs/config.js')
+	.then('models')
+	.then('routes')
+	.into(app);
+
+app.use((req, res) => {
+	res.status(404);
+	res.json({'msg': '404 not found'});
+});
+
+app.use((error, req, res) => {
+	res.status(500);
+	res.json({'msg': `500 ${error}`});
+});
+
+server.listen(app.get('port'), () => {
+	console.log(`SocketPO running on port ${app.get('port')}`);
+});
+
+module.exports = app;

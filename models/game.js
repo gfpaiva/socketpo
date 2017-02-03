f23bda4e5d31dd5daa9ededad5d40b06bc591b9d
gfpaiva <gf.paiva@yahoo.com.br>
2017-02-03 22:02:40 +0000

    game db model

--- models/game.js
+++ models/game.js
@@ -2,9 +2,22 @@ module.exports = (app) => {
 	const db = require('./../libs/db')(),
 		mongoose = require('mongoose'),
 		Schema = mongoose.Schema,
+		player = new Schema({
+			name: { type: String, default: 'Guest' },
+			plays: [ { type: Number, default: 0 } ]
+		});
 		game = new Schema({
 			name: { type: String, required: true },
 			hash: { type: String, unique: true },
+			status: { type: Number, default: 0 },
+			players: [player],
+			result: [{
+				message: { type: String },
+				won: [player],
+				rounds: [{
+
+				}]
+			}]
 			createdAt: { type: Date, default: Date.now }
 		});
 

16ef3bf8256afda504a40583d95e2f6ba328c742
Guilherme Paiva <gf.paiva@yahoo.com.br>
2019-04-10 19:22:03 -0300

    chore: "single" test file

--- client/src/Pages/Single/Single.test.js
+++ client/src/Pages/Single/Single.test.js
@@ -4,18 +4,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { MockedProvider } from 'react-apollo/test-utils';
 import wait from 'waait';
 import Single from './Single';
-import { getGame } from '../../Utils/graphqlAPI';
+import { getGame, gameSub } from '../../Utils/graphqlAPI';
 import { fakeGame, fakePlayer, fakeRound } from '../../Utils/testUtils';
 
 const game = fakeGame(
-	false,
+	true,
 	fakePlayer(),
 	[
 		fakeRound(),
 		fakeRound({ winner: fakePlayer({ id: 'player02', name: 'Player 02' }) }),
 		fakeRound({ isDraw: true })
-	]
+	],
+	{ status: 1 }
 );
+const player = fakePlayer();
+
 const mocks = [
 	{
 		request: { query: getGame, variables: { hash: game.hash } },
@@ -23,6 +26,22 @@ const mocks = [
 			data: {
 				GameByHash: {
 					...game
+				}
+			}
+		},
+	},
+	{
+		request: { query: gameSub, variables: { hash: game.hash } },
+		result: {
+			data: {
+				gameSubscription: {
+					__typename: "GameSub",
+					game : {
+						...game
+					},
+					message: '',
+					hash: game.hash,
+					player: player.name
 				}
 			}
 		},
@@ -42,9 +61,13 @@ describe('<Single />', () => {
 			</Router>
 		);
 
+		const SingleInstance = wrapper.find('Single').instance();
+
+		SingleInstance.currentPlayer = player;
+
 		await wait();
 		wrapper.update();
 
-		console.log(wrapper.debug());
+		expect(wrapper.find('.single__split').length).toBe(1);
 	});
 });
